7fff30eb08f236e058c3b6ed9bbd1d5d4e006935
ATarsa <agnieszkatarsa@gmail.com>
2019-10-14 19:38:30 +0100

    Add react build

---
+++ build/precache-manifest.b1e163cccebb64d717066b22896402ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94fdd3c94d4b5b84a772d7340b1dbce4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "086f1398433a40fbd8e3",
+    "url": "/static/css/main.0a376368.chunk.css"
+  },
+  {
+    "revision": "4aebb5fec1ddcfda7796",
+    "url": "/static/js/2.3d48d06f.chunk.js"
+  },
+  {
+    "revision": "086f1398433a40fbd8e3",
+    "url": "/static/js/main.0c738503.chunk.js"
+  },
+  {
+    "revision": "ae692e0c0fbf548af1b9",
+    "url": "/static/js/runtime-main.f2537ca5.js"
+  }
+]);
---
+++ build/precache-manifest.fa566404b2e20282902276278fff666e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "256a299c6a2675ba2b2529c3ced64dab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf7509672175106767f6",
+    "url": "/static/css/main.0a376368.chunk.css"
+  },
+  {
+    "revision": "4aebb5fec1ddcfda7796",
+    "url": "/static/js/2.3d48d06f.chunk.js"
+  },
+  {
+    "revision": "cf7509672175106767f6",
+    "url": "/static/js/main.59697412.chunk.js"
+  },
+  {
+    "revision": "ae692e0c0fbf548af1b9",
+    "url": "/static/js/runtime-main.f2537ca5.js"
+  }
+]);
