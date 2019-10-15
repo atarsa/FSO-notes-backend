dea9d8caa4f4f5ebad4a29c23c27081c55b6c654
ATarsa <agnieszkatarsa@gmail.com>
2019-10-15 19:04:03 +0100

    uibuild

--- index.js
+++ index.js
@@ -23,15 +23,11 @@ let notes = [
      }]
 
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
-
-app.get('/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
   res.json(notes)
 })
 
-app.get('/notes/:id', (req, res) => {
+app.get('/api/notes/:id', (req, res) => {
   const id = Number(req.params.id)
   const note = notes.find(note => note.id === id)
 
@@ -42,7 +38,7 @@ app.get('/notes/:id', (req, res) => {
   }  
 })
 
-app.delete('/notes/:id', (req, res) => {
+app.delete('/api/notes/:id', (req, res) => {
   const id = Number(req.params.id)
   notes = notes.filter(note => note.id !== id)
 
@@ -55,7 +51,7 @@ const generateId = () => {
     : 0
 }
 
-app.post('/notes', (req, res) => {
+app.post('/api/notes', (req, res) => {
   const body = req.body
 
   if (!body.content){
